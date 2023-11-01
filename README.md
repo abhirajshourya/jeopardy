aa7e4700c27df1e5970388b74367f4d93408943c
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2023-10-31 23:47:41 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jeopardy
+
+A Project for INFO-6122-(02)-23F for Fanshawe College, Program MAP-1 by
+1. Abhiraj Shourya
+2. Nhut Tan Le
---
+++ js/abhiraj_tan.js
@@ -0,0 +1,16 @@
+/**
+ * A Project by Abhiraj Shourya & Nhut Tan Le
+ * For INFO-6122-(02)-23F in Fanshawe College, MAP program.
+ */
+
+/**
+ * Function to create the Welcome Page of the Game.
+ */
+
+/**
+ * Function to create the Jeopardy Board.
+ */
+
+/**
+ * Class to create & start the Jeopardy Game.
+ */
